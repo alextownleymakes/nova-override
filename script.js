4eaf533a8e7ca481d2a2489c1537b0b7fc848b93
alextownleymakes <alextownleymakes@gmail.com>
2019-09-30 08:46:29 -0500

    weekend update 1

--- script.js
+++ script.js
@@ -9,7 +9,7 @@ let angle = 360;
 let handling = 8;
 let acceleration = 3;
 let deceleration = .2;
-let globalSpeedCap = 20;
+let globalSpeedCap = 10;
 var kdr = 1;
 var leftTurn = false;
 var accelRatio = 1;
@@ -18,8 +18,23 @@ var forward = false;
 var stopMovement = false;
 var maxBullets = 20;
 var bulletSpeed = 20;
-var maxEnemies = 5;
 var showBounding = false;
+var starCount = 2000;
+var explosionCount = 20;
+var explosions = {
+
+};
+
+
+var maxEnemies = 5;
+var enemiesThisWave = 5;
+var spawnCount = 0;
+var waveKill = 0;
+var killCount = 0;
+var waveCount = 1;
+var theScore = 0;
+
+
 setInterval(update, 1000/fps);
 
 var ship = {
@@ -39,10 +54,20 @@ var ship = {
 }
 var enemy = {
     ships: [],
+
 }
+var stars = [];
+
+
+////////////////////////////////
+////////// GAMEPLAY!! //////////
+////////////////////////////////
 function update() {
     calculateAccelRatio(ship.thrust.x,ship.thrust.y,deceleration);
     spaceDraw();
+    starsPlace();
+    starsDraw();
+    overlayRefresh();
     shipDraw();
     // thrusterDraw();
     shipRotation();
@@ -58,11 +83,48 @@ function update() {
     enemyDraw();
     enemyAcceleration();
     clearEnemies();
+    killEnemy();
+    waveIncrease();
+}
+
+function overlayRefresh() {
+    document.getElementById('wave').innerHTML=(waveCount);
+    document.getElementById('score').innerHTML=(theScore);
+    document.getElementById('kills').innerHTML=(killCount);
+}
+////////////////////////////////
+////////// BACKGROUND //////////
+////////////////////////////////
+function starsPlace() {
+    for (i = 0; stars.length < starCount; i++) {
+        stars.push ({
+            x: Math.random()*canvas.width,
+            y: Math.random()*canvas.height
+        });
+    }
+}
+function starsDraw() {
+    for (i = 0; i < stars.length; i++) {
+        c.strokeStyle = "white";
+        c.lineWidth = "1";
+        c.beginPath();
+
+        c.arc(stars[i].x,stars[i].y,.3,0,Math.PI*2,false);
+        c.stroke();
+    }
 }
 function spaceDraw () {
     c.fillStyle = "black";
     c.fillRect (0, 0, canvas.width, canvas.height);
+    
+
+   
 }
+
+
+/////////////////////////////////
+////////// PLAYER SHIP //////////
+/////////////////////////////////
 function shipDraw() {
     c.strokeStyle = "white",
     c.lineWidth = shipSize / 10;
@@ -171,8 +233,33 @@ function thrustLimiter(x,y,s) {
     if (y >= s) { ship.thrust.y = s-.5};
     if (y <= -s) { ship.thrust.y = -s+.5};
 }
+function distanceBetween (x1,y1,x2,y2) {
+    return Math.sqrt(Math.pow(x2-x1, 2) * Math.pow(y2 - y1, 2));
+}
+function explosion(dis) {
+    for(k=0; k < explosionCount; k++) {
+        enemy.ships[j].explosions.push({
+            x: dis.x,
+            y: dis.y,
+        });
+        for(l=0; l < dis.explosions.length; l++){
+            c.beginPath()
+            c.strokeStyle = "red";
+            c.fillStyle = "orange";
+            c.arc((dis.x-20)+Math.random()*40,(dis.y-20)+Math.random()*40,dis.r,0,Math.PI*2,false);
+            c.fill();
+            c.stroke();
+            c.closePath();
+        }
+    // enemy.ships.splice(j,1);  
+    }
+}
+
+////////////////////////////////
+////////// ENEMY SHIP //////////
+////////////////////////////////
 function enemyCreate() {
-    if (enemy.ships.length < maxEnemies) {
+    while (enemy.ships.length < maxEnemies) {
         enemy.ships.push({
             x: Math.random() * canvas.width,
             y: Math.random() * canvas.height,
@@ -187,8 +274,11 @@ function enemyCreate() {
             speedCap: globalSpeedCap*.7,
             fireGun: false,
             bullets: [],
+            explosions: [],
         });
+        spawnCount++;
     }
+
 }
 function enemyDraw () {
     for (var i = 0; i < enemy.ships.length; i++) {
@@ -229,8 +319,8 @@ function enemyAcceleration () {
         }
         // if ( )
         // console.log(ship.thrust.y);
-    enemy.ships[i].x += (enemy.ships[i].thrust.x*.5);
-    enemy.ships[i].y += (enemy.ships[i].thrust.y*.5);
+    enemy.ships[i].x += (enemy.ships[i].thrust.x*.2);
+    enemy.ships[i].y += (enemy.ships[i].thrust.y*.2);
     } 
 }
 function clearEnemies() {
@@ -267,26 +357,29 @@ function calculateAccelRatio (x,y,d) {
         accelRatio = kdr*x;
     }
 }
+
+
+////////////////////////////////
+////////// THE COMBAT //////////
+////////////////////////////////
 function bulletsShoot() { 
     if (ship.fireGun && ship.bullets.length < maxBullets) {
         ship.bullets.push({
             x: ship.x + 4/3 + ship.r * Math.cos(ship.a),
             y: ship.y - 4/3 * ship.r * Math.sin(ship.a),
             xv: bulletSpeed * Math.cos(ship.a),
-            yv: -bulletSpeed * Math.sin(ship.a)
-    }); 
-}
-// console.log(ship.bullets);
-    ship.fireGun = false;
+            yv: -bulletSpeed * Math.sin(ship.a),
+            r: 3,
+        }); 
+    }
+    // ship.fireGun = false;
 }
 function bulletsDraw () {
     for (var i = 0; i < ship.bullets.length; i++) {
         c.fillstyle = "purple";
         c.beginPath();
-        c.arc(ship.bullets[i].x,ship.bullets[i].y, 5, 0, Math.PI * 2, false);
+        c.arc(ship.bullets[i].x,ship.bullets[i].y, ship.bullets[i].r, 0, Math.PI * 2, false);
         c.fill();
-
-
     }
 }
 function bulletsMove() {
@@ -306,6 +399,40 @@ function clearBullets() {
         }
     }
 }
+function killEnemy() {
+    var explodey = false;
+    var whichExplodey;
+    for (i = 0; i < ship.bullets.length; i++) {
+        for(j = 0; j < enemy.ships.length; j++) {
+            if (distanceBetween(ship.bullets[i].x,ship.bullets[i].y,enemy.ships[j].x,enemy.ships[j].y) < ship.bullets[i].r+enemy.ships[j].r) {
+                // enemy.ships.splice(j,1);
+                // explosion(enemy.ships[j]);
+                killCount++;
+                waveKill++;
+                whichExplodey = enemy.ships[j];
+                explosion(whichExplodey);
+                enemy.ships.splice(j,1);
+                theScore+= (10*(waveCount/2))+5;
+            }
+        }
+    }
+}
+
+
+
+////////////////////////////////
+////////// THE COMBAT //////////
+////////////////////////////////
+
+function waveIncrease() {
+    if (waveKill == enemiesThisWave) {
+        waveCount++;
+        enemiesThisWave+=5;
+        waveKill = 0;
+        maxEnemies++;
+    }
+}
+
 //keyboard movement
 function keyboardMovement()
 {
@@ -399,7 +526,7 @@ function keyboardMovement()
 			//key Space
 			case 32:
 
-			fireGun = false;
+			ship.fireGun = false;
 
 			break;
 		}
